4e654979225009122762238e65bd51609a16016f
DerDanilo <DerDanilo@users.noreply.github.com>
2019-02-11 21:11:21 +0100

    Update disable-verbose-output.sh

--- disable-verbose-output.sh
+++ disable-verbose-output.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # Debian based
-echo "Disable verbose SNMP output"
+echo "Disable verbose SNMP output - Ignore possible sed errormessages"
 sed -i "s|-LS3d|-LS6d|" /etc/default/snmpd
 sed -i "s|-LS3d|-LS6d|" /lib/systemd/system/snmpd.service
 sed -i "s|-Lsd|-LS6d|" /etc/default/snmpd
