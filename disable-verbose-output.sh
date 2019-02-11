ecb30a2a8824e03668b4168be76eae017cb4da99
DerDanilo <DerDanilo@users.noreply.github.com>
2019-02-11 20:55:28 +0100

    Create disable-verbose-output.sh

---
+++ disable-verbose-output.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+# Debian based
+echo "Disable verbose SNMP output
+sed -i "s|-LS3d|-LS6d|" /etc/default/snmpd
+sed -i "s|-LS3d|-LS6d|" /lib/systemd/system/snmpd.service
+sed -i "s|-Lsd|-LS6d|" /etc/default/snmpd
+sed -i "s|-Lsd|-LS6d|" /lib/systemd/system/snmpd.service
+echo "Reload systemd daemon"
+systemctl daemon-reload
+echo "Restart snmpd"
+service snmpd restart
